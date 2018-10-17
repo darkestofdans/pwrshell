8ae007200ae9e5589447ecac0fe86a26676a564a
darkestofdans <32725937+darkestofdans@users.noreply.github.com>
2018-10-16 21:11:32 -0500

    reboot_events_2h.ps1 added cmd command
    Modified the file to include the cmd command to execute the Powershell command.

--- reboot_events_2h.ps1
+++ reboot_events_2h.ps1
@@ -1,2 +1,7 @@
-#Get-EventLog for the last 2 hours for EventIDs associated with reboots - 6005, 6006, 6008, 6009, 1074
-Get-EventLog system -after (get-date).Addhours(-2) | where {$_.eventID -match '[16]0[07][4-9]'} | format-list
+#Powershell command get reboot events from Event Log for the past two hours for 6005, 6006, 6008, 6009, 1074, 1076.
+Get-EventLog system -after (get-date).AddHours(-2) | where {$_.EventID -match '[16]0[07][4-9]'} | format-list}
+
+#Powershell command to execute from cmd to get reboot events from Event Log for the past two hours for 6005, 6006, 6008, 6009, 1074, 1076
+#copy everything from powershell.exe to the end.
+
+#powershell.exe -Command "& {Get-EventLog system -after (get-date).AddHours(-2) | where {$_.EventID -match '[16]0[07][4-9]'} | format-list}"
