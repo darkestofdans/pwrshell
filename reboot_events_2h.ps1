cbaba7a4781e59031173d5a2daa5c70e8032d4e5
darkestofdans <32725937+darkestofdans@users.noreply.github.com>
2018-10-23 10:59:43 -0500

    reboot_events_2h.ps1
    How'd that bracket get there?!  Fixed.

--- reboot_events_2h.ps1
+++ reboot_events_2h.ps1
@@ -1,5 +1,5 @@
 #Powershell command get reboot events from Event Log for the past two hours for 6005, 6006, 6008, 6009, 1074, 1076.
-Get-EventLog system -after (get-date).AddHours(-2) | where {$_.EventID -match '[16]0[07][4-9]'} | format-list}
+Get-EventLog system -after (get-date).AddHours(-2) | where {$_.EventID -match '[16]0[07][4-9]'} | format-list
 
 #Powershell command to execute from cmd to get reboot events from Event Log for the past two hours for 6005, 6006, 6008, 6009, 1074, 1076
 #copy everything from powershell.exe to the end.
